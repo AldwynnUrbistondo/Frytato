d8ed0eeb5d45b6d860ee5c2b2a37a4863945e2f0
rfrias101 <friasprinze@gmail.com>
2025-09-20 01:17:52 +0800

    Plant

---
+++ Frytato/Assets/Script/Soil.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Soil : MonoBehaviour, IInteractable
+{
+    public Plant plant;
+    [SerializeField] PlantState plantstate = PlantState.Empty;
+
+
+    public void Interact()
+    {
+        if (plantstate == PlantState.Empty)
+        {
+            Debug.Log("Planted Soil");
+            Plant();
+        }
+        else if (plantstate == PlantState.Growing)
+        {
+            Debug.Log("There is a plant growing");
+        }
+        else if (plantstate == PlantState.Harvest)
+        {
+            Harvest();
+        }
+    }
+
+    void Update()
+    {
+        if (plant.isGrowing)
+        {
+            Plant();
+        }
+    }
+
+    void Plant()
+    {
+        plant.growDuration += Time.deltaTime;
+        plant.isGrowing = true;
+        Debug.Log(plant.growDuration);
+        plantstate = PlantState.Growing;
+        if (plant.growDuration >= 10)
+        {
+            Debug.Log("Plant is done growing");
+            plant.isGrowing = false;
+            plant.growDuration = 0;
+            plantstate = PlantState.Harvest;
+        }
+    }
+
+    void Harvest()
+    {
+        plant.harvestAmount = Random.Range(1, 3);
+        Debug.Log($"You have harvested {plant.harvestAmount} of potatoes.");
+        plantstate = PlantState.Empty;
+    }
+
+
+}
+
+public enum PlantState
+{
+    Empty,
+    Growing,
+    Harvest
+}
+[System.Serializable]
+public class Plant
+{
+    public float growDuration;
+    public bool isGrowing = false;
+    public int harvestAmount;
+    public int potatoRarity;
+}
---
+++ Frytato/Assets/Script/UIManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+}
