6eed5ba80207718e49aedd4649526839000c3746
AldwynnUrbistondo <aldwynn020505@gmail.com>
2025-09-19 23:27:09 +0800

    .

--- Frytato/Assets/Script/Player/Interact.cs
+++ Frytato/Assets/Script/Player/Interact.cs
@@ -2,57 +2,14 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    [SerializeField] float radius = 0.5f;      // Radius of the sphere
-    [SerializeField] float distance = 3f;      // Max distance
-    [SerializeField] LayerMask interactableMask;
-
-    private RaycastHit hit;
-    private IInteractable currentTarget;       // the object we're currently aiming at
-
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        // --- Cast a "fat ray" ---
-        bool hasHit = Physics.SphereCast(transform.position, radius, transform.forward, out hit, distance, interactableMask);
-
-        if (hasHit)
+        IInteractable interactable = other.GetComponent<IInteractable>();
+        if(interactable != null)
         {
-            Debug.Log("Hit " + hit.collider.name);
-            var interactable = hit.collider.GetComponent<IInteractable>();
-
-            // ENTER
-            if (interactable != null && interactable != currentTarget)
-            {
-                // We were pointing at something new
-                currentTarget?.OnRaycastExit();   // call exit on old
-                currentTarget = interactable;
-                currentTarget.OnRaycastEnter();
-            }
-
-            // STAY
-            if (interactable == currentTarget)
-            {
-                currentTarget.OnRaycastStay();
-
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    currentTarget.Interact();
-                }
-            }
-        }
-        else
-        {
-            // EXIT if we had something last frame but now nothing
-            if (currentTarget != null)
-            {
-                currentTarget.OnRaycastExit();
-                currentTarget = null;
-            }
+            interactable.Interact();
         }
     }
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position + transform.forward * distance, radius);
-    }
+
 }
